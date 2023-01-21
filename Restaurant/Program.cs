ce5bdf35796a25099c8878ccdd96dcfce15522ee
CYKERO4KA <cykero4ka88@gmail.com>
2023-01-21 17:43:40 +0200

    Add bool field

---
+++ Restaurant/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace Project
+{
+    class Start
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+
+    class Restaurant
+    {
+        private List<Tables> _tables;
+        private Queue<Customer> _customers;
+        private Dictionary<Menu, int> menu;
+    }
+
+    class Customer
+    {
+        private int _money;
+        public int ReserveTime { get; private set; }
+
+        public Customer(int reserveTime)
+        {
+            ReserveTime = reserveTime;
+        }
+    }
+
+    class Menu
+    {
+        
+    }
+
+    class Tables
+    {
+        public bool isReserved;
+    }
+}
