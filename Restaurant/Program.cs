c149d06779009e600fa42bb57d367ef7406bd04f
CYKERO4KA <cykero4ka88@gmail.com>
2023-01-22 13:42:48 +0200

    Final

--- Restaurant/Program.cs
+++ Restaurant/Program.cs
@@ -4,9 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Menu menu = new Menu();
-            menu.ShowMenu();
-            Console.ReadKey();
+            Restaurant restaurant = new Restaurant(7);
+            restaurant.OpenRestaurant();
         }
     }
 
@@ -14,6 +13,7 @@
     {
         private List<Table> _tables = new List<Table>();
         private Queue<Customer> _customers = new Queue<Customer>();
+        private int _money;
         
 
         private Menu menu = new Menu();
@@ -21,15 +21,92 @@
         public Restaurant(int tableCount)
         {
             Random random = new Random();
-            for(int i = 0; i < tableCount; i++)
-                _tables.Add(new Table(15));
+            for (int i = 0; i < tableCount; i++)
+            {
+                _tables.Add(new Table(random.Next(5, 15)));
+            }
+            CreateNewCustomer(25, random);
+        }
+
+        public void CreateNewCustomer(int count, Random random)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                _customers.Enqueue(new Customer(random.Next(100,250)));
+            }
         }
 
         public void OpenRestaurant()
         {
-            
+            while (_customers.Count > 0)
+            {
+                Random random = new Random();
+                Customer newCustomer = _customers.Dequeue();
+                Console.WriteLine($"Balance of restaurant: {_money}$. Wait for a new Client");
+                Console.WriteLine($"You have a new client, he wants to buy {menu.dishes[random.Next(1,10)]}");
+                ShowAllTablesStates();
+
+                Console.Write("\nYou advice him table: ");
+                string userInput = Console.ReadLine();
+                if (Int32.TryParse(userInput, out int tableNumber))
+                {
+                    tableNumber -= 1;
+
+                    if (tableNumber >= 0 && tableNumber < _tables.Count)
+                    {
+                        if (_tables[tableNumber].IsReserved)
+                        {
+                            Console.WriteLine("You tried to pick a client, but here is no empty place. He argued and leave your restaurant! ");
+                        }
+                        else
+                        {
+                            if (newCustomer.CheckSolvency(_tables[tableNumber]))
+                            {
+                                Console.WriteLine($"Client pay computer cost. And sit at the table {tableNumber+1}");
+                                _money = newCustomer.Pay();
+                                _tables[tableNumber].BecomeReserved(newCustomer);
+                            }
+                            else
+                            {
+                                Console.WriteLine("Client can't pay money!");
+                            }
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("You can't replace client! He argued you and leave your restaurant!");
+                    }
+                }
+                else
+                {
+                    CreateNewCustomer(1, new Random());
+                    Console.WriteLine("Try again!");
+                }
+                
+                Console.WriteLine("Press any key, to talk with a new client");
+                Console.ReadKey();
+                Console.Clear();
+                SpendOneMinute();
+            }
         }
 
+        public void ShowAllTablesStates()
+        {
+            Console.WriteLine("\nList of all tables: ");
+            for (int i = 0; i < _tables.Count; i++)
+            {
+                Console.Write(i +1+" - ");
+                _tables[i].ShowInfo();
+            }
+        }
+
+        private void SpendOneMinute()
+        {
+            foreach (var table in _tables)
+            {
+                table.SpendOneMinute();
+            }
+        }
     }
 
     class Customer
@@ -77,22 +154,17 @@
             {8,"Ice-cream"},
             {9,"Hamburger"}
         };
-
-        public void ShowMenu()
-        {
-            for(int i =1; i<10; i++)
-                Console.WriteLine(dishes[i]);
-        }
+        
     }
 
     class Table
     {
+        private Customer _customer;
+        private int _minutesRemaining;
         public bool IsReserved
         {
             get => _minutesRemaining > 0;
         }
-        private Customer _customer;
-        private int _minutesRemaining;
         
         public int PricePerMinute { get; private set; }
 
@@ -121,6 +193,8 @@
         {
             if(IsReserved)
                 Console.WriteLine($"This table is reserved, wait: {_minutesRemaining} minute");
+            else
+                Console.WriteLine($"Table isn't taken, it costs: {PricePerMinute}$ ");
         }
     }
 }
